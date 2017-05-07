ccbd865eb3a3b6a9cd4c56cc4d601d61f0ec7dec
hoshin <martin.bahier@gmail.com>
2017-05-07 15:41:53 +0200

    Revamp links list

--- client/src/components/PostsListWithData.js
+++ client/src/components/PostsListWithData.js
@@ -7,26 +7,6 @@ import {
 import UpvotePostMutation from './DownvotePost';
 import DownvotePostMutation from './UpvotePost';
 
-
-const PostsList = ({ data: { loading, error, posts }, mutate }) => {
-    if (loading) {
-        return <p>Loading ...</p>;
-    }
-    if (error) {
-        return <p>{error.message}</p>;
-    }
-
-    return (
-        <div className="postsList">
-            <div className="section-title">List of links</div>
-            { posts.map(post => <div key={post.id} className="post">
-                <a href={post.pageURL}>{post.pageTitle || 'No title available'}</a> by {post.posterNick}
-                <span className="upvotes">{`(${post.upvotes} upvotes)`} <UpvotePostMutation className="vote up" postId={post.id}/> <DownvotePostMutation className="vote down" postId={post.id}/></span>
-            </div>) }
-        </div>
-    );
-};
-
 const postsListQuery = gql`
   query PostsListQuery {
     posts {
@@ -38,5 +18,24 @@ const postsListQuery = gql`
     }
   }
 `;
+
+const PostsList = ({ data: { loading, error, posts }, mutate }) => {
+    if (loading) {
+        return <p>Loading ...</p>;
+    }
+    if (error) {
+        return <p>{error.message}</p>;
+    }
+
+    return (
+        <div className="postsList">
+            <div className="section-title">Saved links</div>
+            { posts.map(post => <div key={post.id} className="post">
+                <a href={post.pageURL}>{post.pageTitle || 'No title available'}</a> by {post.posterNick}
+                <span className="upvotes">{`(${post.upvotes} upvotes)`} <UpvotePostMutation className="vote up" postId={post.id}/> <DownvotePostMutation className="vote down" postId={post.id}/></span>
+            </div>) }
+        </div>
+    );
+};
 
 export default graphql(postsListQuery)(PostsList);
